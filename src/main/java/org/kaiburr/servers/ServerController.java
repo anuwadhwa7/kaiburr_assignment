8812f39ef0508878d55ede0ce1fe11d7fe61b527
Anurag Wadhwa <anurag.wadhwa7@gmail.com>
2020-03-25 21:21:50 +0530

    init commit

---
+++ src/main/java/org/kaiburr/servers/ServerController.java
@@ -0,0 +1,12 @@
+package org.kaiburr.servers;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ServerController {
+
+
+
+
+}
